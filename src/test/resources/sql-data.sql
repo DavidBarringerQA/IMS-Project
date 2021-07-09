f72037a3657bfa33259138182c97a9cfd920705a
DavidBarringerQA <DBarringer@qa.com>
2021-07-09 15:12:00 +0100

    IMS-23 #comment A series of changes to make files fit tests

--- src/test/resources/sql-data.sql
+++ src/test/resources/sql-data.sql
@@ -1,2 +1,2 @@
-INSERT INTO `customers` (`first_name`, `surname`) VALUES ('jordan', 'harrison');
-INSERT INTO `item` (`name`, `value`) VALUES ('ball', 1.00);
+INSERT INTO `customers` (`forename`, `surname`) VALUES ('jordan', 'harrison');
+INSERT INTO `items` (`name`, `value`) VALUES ('ball', 1.00);
