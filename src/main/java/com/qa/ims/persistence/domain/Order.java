b8fa29484f2292afb1b05557fe7b79bf2a00998f
DavidBarringerQA <DBarringer@qa.com>
2021-07-12 14:12:18 +0100

    IMS-11 #comment Added Order object

---
+++ src/main/java/com/qa/ims/persistence/domain/Order.java
@@ -0,0 +1,98 @@
+package com.qa.ims.persistence.domain;
+
+import java.util.List;
+
+public class Order {
+
+  private Long id;
+  private Customer customer;
+  private List<Item> items;
+
+  public Order(Customer customer, List<Item> items){
+    this.customer = customer;
+    this.items = items;
+  }
+
+  public Order(Long id, Customer customer, List<Item> items){
+    this.id = id;
+    this.customer = customer;
+    this.items = items;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public Customer getCustomer() {
+    return customer;
+  }
+
+  public void setCustomer(Customer customer) {
+    this.customer = customer;
+  }
+
+  public List<Item> getItems() {
+    return items;
+  }
+
+  public void setItems(List<Item> items) {
+    this.items = items;
+  }
+
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((customer == null) ? 0 : customer.hashCode());
+    result = prime * result + ((id == null) ? 0 : id.hashCode());
+    result = prime * result + ((items == null) ? 0 : items.hashCode());
+    return result;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    Order other = (Order) obj;
+    if (customer == null) {
+      if (other.customer != null)
+	return false;
+    } else if (!customer.equals(other.customer))
+      return false;
+    if (id == null) {
+      if (other.id != null)
+	return false;
+    } else if (!id.equals(other.id))
+      return false;
+    if (items == null) {
+      if (other.items != null)
+	return false;
+    } else if (!items.equals(other.items))
+      return false;
+    return true;
+  }
+
+  @Override
+  public String toString() {
+    String res = "Order id: " + this.id + "\nCustomer: " + customer.getFirstName() + " " + customer.getSurname() + "\nItems: ";
+    for(Item item : items){
+      res += "\t" + item.getName() + "\t" + item.getValue();
+    }
+    return res;
+  }
+
+  public double getTotal() {
+    return items.stream()
+      .mapToDouble(x -> x.getValue())
+      .sum();
+  }
+  
+}
